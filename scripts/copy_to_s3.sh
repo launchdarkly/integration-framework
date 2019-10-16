539f2fad328d3c2d9332c43e8a8a970a93b71da6
Rich Manalang <rmanalang@launchdarkly.com>
2019-10-16 13:02:22 -0700

    Write `latest` sha to S3

--- scripts/copy_to_s3.sh
+++ scripts/copy_to_s3.sh
@@ -1,2 +1,4 @@
+echo $CIRCLE_SHA1 > latest
 aws s3 rm --recursive s3://launchdarkly-integrations-dev/integrations
 aws s3 cp --recursive integrations s3://launchdarkly-integrations-dev/integrations
+aws s3 cp latest s3://launchdarkly-integrations-dev/integrations
