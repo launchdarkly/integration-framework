829f708699dd98d05654b1f83cdd42b7d213971d
Henry Barrow <hbarrow@launchdarkly.com>
2019-10-16 16:05:03 -0700

    Migrate to production s3

--- scripts/copy_to_s3.sh
+++ scripts/copy_to_s3.sh
@@ -1,4 +1,5 @@
-echo $CIRCLE_SHA1 > latest
-aws s3 rm --recursive s3://launchdarkly-integrations-dev/integrations
-aws s3 cp --recursive integrations s3://launchdarkly-integrations-dev/integrations
-aws s3 cp latest s3://launchdarkly-integrations-dev/integrations/latest.txt
+#!/bin/bash
+REV=$(git rev-parse HEAD | cut -c1-8)
+echo $REV > latest
+aws s3 cp --recursive integrations s3://$BUCKET_NAME/integrations/$REV
+aws s3 cp latest s3://$BUCKET_NAME/latest.txt
