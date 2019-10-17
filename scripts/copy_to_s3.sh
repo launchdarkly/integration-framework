788d7a116b5eabdbce5b39328f64b75439c42e34
Rich Manalang <rmanalang@launchdarkly.com>
2019-10-17 15:57:11 -0700

    One more fix

--- scripts/copy_to_s3.sh
+++ scripts/copy_to_s3.sh
@@ -1,4 +1,5 @@
-REV=$(git rev-parse HEAD | cut -c1-8)#!/bin/bash
+#!/bin/bash
+REV=$(git rev-parse HEAD | cut -c1-8)
 python -c 'import glob, json, os; print json.dumps({"sha": os.environ["REV"], "integrations": [dir.split("/")[1] for dir in glob.glob("**/*/manifest.json")]})' > latest
 aws s3 cp --recursive integrations s3://$BUCKET_NAME/integrations/$REV
 aws s3 cp latest s3://$BUCKET_NAME/latest.json
