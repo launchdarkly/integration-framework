9f5200c1ad1aea19b3ba38ca5f69e12b6fbbbf07
Rich Manalang <rmanalang@launchdarkly.com>
2019-10-17 16:07:26 -0700

    s/public/public-read

--- scripts/copy_to_s3.sh
+++ scripts/copy_to_s3.sh
@@ -2,4 +2,4 @@
 export REV=$(git rev-parse HEAD | cut -c1-8)
 python -c 'import glob, json, os; print json.dumps({"sha": os.environ["REV"], "integrations": [dir.split("/")[1] for dir in glob.glob("**/*/manifest.json")]})' > latest
 aws s3 cp --recursive integrations s3://$BUCKET_NAME/integrations/$REV
-aws s3 cp latest s3://$BUCKET_NAME/latest.json --acl public
+aws s3 cp latest s3://$BUCKET_NAME/latest.json --acl public-read
