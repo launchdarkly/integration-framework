6819ff53f66e21ad3c7328f9feb2e147b41f26d1
Henry Barrow <hbarrow@launchdarkly.com>
2020-03-27 14:02:46 +0000

    Add parser schema, sampleResponse, and tests (#64)
    * Add support for split files and move reserved custom properties
    
    * Move auditLogEvents hook to new file
    
    * Add external condition to manifest and add sample external condition to sample-integration
    
    * remove test file
    
    * Usse JSON pointer in description
    
    * Move manifest schema up a directory
    
    * Add parser schema to parser script and update VS code settings
    
    * Update url
    
    * Add sample response
    
    * Add JSON pointer tests
    
    * Add tests for template file
    
    * Address PR feedback
    
    * Adjust template regex pattern
    
    * Remove arbitrary minLength
    
    * Move parser into manifest

--- bundleSchemas.js
+++ bundleSchemas.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const $RefParser = require('@apidevtools/json-schema-ref-parser');
 
-const baseSchema = require('./schemas/base.json');
+const manifestBaseSchema = require('./schemas/base.json');
 
 const bundle = async () => {
   try {
-    const schema = await $RefParser.bundle(baseSchema);
+    const manifestSchema = await $RefParser.bundle(manifestBaseSchema);
     fs.writeFileSync(
       'manifest.schema.json',
-      JSON.stringify(schema, null, 2) + '\n'
+      JSON.stringify(manifestSchema, null, 2) + '\n'
     );
   } catch (e) {
     console.error(e);
