81ab998d8bf2635f91e2002508bb99246b8ddebc
Henry Barrow <hbarrow@launchdarkly.com>
2021-09-27 13:39:53 +0100

    Add cache control headers to integrations svgs (#290)

--- scripts/copy_to_s3.sh
+++ scripts/copy_to_s3.sh
@@ -2,6 +2,6 @@
 export REV=$(git rev-parse HEAD | cut -c1-8)
 python -c 'import glob, json, os; print json.dumps({"sha": os.environ["REV"], "integrations": [dir.split("/")[1] for dir in glob.glob("**/*/manifest.json")]})' > latest
 aws s3 cp --recursive integrations s3://$BUCKET_NAME/integrations/$REV
-aws s3 cp --recursive --exclude "*" --include "*.svg" integrations s3://launchdarkly-static/integrations/$REV
+aws s3 cp --recursive --exclude "*" --include "*.svg" integrations s3://launchdarkly-static/integrations/$REV --cache-control max-age=31536000
 aws s3 cp manifest.schema.json s3://$BUCKET_NAME/integrations/$REV/manifest.schema.json --metadata-directive REPLACE --content-type "application/json"
 aws s3 cp latest s3://$BUCKET_NAME/latest.json --metadata-directive REPLACE --content-type "application/json"
