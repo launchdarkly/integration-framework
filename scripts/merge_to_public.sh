0228a1d95e88f5e4588d6840e23bdac40098d3b3
Henry Barrow <hbarrow@launchdarkly.com>
2023-07-18 16:52:05 +0100

    Releasing changes publicly

--- scripts/merge_to_public.sh
+++ scripts/merge_to_public.sh
@@ -3,14 +3,19 @@
 # This script merges forward from the private mirror to the public one,
 # one main branch to the other while squashing commits.
 #
-# Usage: ./merge_to_public.sh <commit_message>
+# Usage: ./merge_to_public.sh <commit_message> <branch_name>
 #
 
 set -x
 
 COMMIT_MSG=${1:-Releasing changes publicly}
+BRANCH_NAME=${2:-$(date +%B-%d-%Y)-release}
+
+# Convert BRANCH_NAME to lowercase
+BRANCH_NAME=$(echo "$BRANCH_NAME" | tr '[:upper:]' '[:lower:]')
 
 echo
+echo "Using branch ${BRANCH_NAME}"
 echo "Merging to the public mirror with message: ${COMMIT_MSG}"
 echo "NOTE: merge conflicts will be automatically resolved by preferring changes from the private mirror."
 echo
@@ -23,7 +28,8 @@ git checkout main
 git fetch private
 git merge private/main --squash --strategy-option=theirs
 git commit -a -m "${COMMIT_MSG}" # Merge conflicts will need to be resolved manually
-git push origin main
+git checkout -b "${BRANCH_NAME}"
+git push --set-upstream origin ${BRANCH_NAME}
 
 cd ../..
 rm -rf temp
